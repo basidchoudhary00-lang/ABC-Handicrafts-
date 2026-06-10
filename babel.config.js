3eaa5ebcb53b9e48323a1c44d58c1afc7908759d
basidchoudhary00 <basidchoudhary00@gmail.com>
2026-06-10 17:47:00 +0530

    fix: added reanimated plugin to babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    presets: [['babel-preset-expo', { unstable_transformProfile: 'hermes-stable' }]],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
+
